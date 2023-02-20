80801d8962fca7920e7ac7580e84a461508d09c2
Milind452 <mohapatra.milind@gmail.com>
2023-02-20 13:43:34 +0530

    fetch tasks from api

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,19 +1,50 @@
 import "./Board.css";
 
 import Lane from "../../components/Lane/Lane";
+import { useEffect, useState } from "react";
+
+const lanes = [
+    { id: 1, title: "To Do" },
+    { id: 2, title: "In Progress" },
+    { id: 3, title: "Review" },
+    { id: 4, title: "Done" },
+];
 
 export default function Board() {
-    const lanes = [
-        { id: 1, title: "To Do" },
-        { id: 2, title: "In Progress" },
-        { id: 3, title: "Review" },
-        { id: 4, title: "Done" },
-    ];
+    const [loading, setLoading] = useState(false);
+    const [tasks, setTasks] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetch(
+                    "https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks"
+                );
+                const result = await data.json();
+                if (result) {
+                    setTasks(result);
+                    setLoading(false);
+                }
+            } catch (e) {
+                setError(e.message);
+                setLoading(false);
+            }
+        }
+        setLoading(true);
+        fetchData();
+    }, []);
 
     return (
         <div className="Board-wrapper">
             {lanes.map((lane) => (
-                <Lane key={lane.id} title={lane.title} />
+                <Lane
+                    key={lane.id}
+                    title={lane.title}
+                    loading={loading}
+                    error={error}
+                    tasks={tasks.filter((task) => task.lane === task.id)}
+                />
             ))}
         </div>
     );
