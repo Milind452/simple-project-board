7b6154760f50513ecc603b43f7dc5896f0ed1a05
Milind452 <mohapatra.milind@gmail.com>
2023-02-20 12:08:51 +0530

    Add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <div className="Header-wrapper">
+            <h1>Project Management Board</h1>
+        </div>
+    );
+}
